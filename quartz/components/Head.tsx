3c5d5d3f0fc37a7d7717d06dcca0822f7d06a11c
Zhang <quan.zhang11@okstate.edu>
2026-06-24 15:44:28 -0500

    Quartz sync: Jun 24, 2026, 3:44 PM

--- quartz/components/Head.tsx
+++ quartz/components/Head.tsx
@@ -4,7 +4,7 @@ import { CSSResourceToStyleElement, JSResourceToScriptElement } from "../util/re
 import { googleFontHref, googleFontSubsetHref } from "../util/theme"
 import { QuartzComponent, QuartzComponentConstructor, QuartzComponentProps } from "./types"
 import { unescapeHTML } from "../util/escape"
-import { CustomOgImagesEmitterName } from "../plugins"
+//import { CustomOgImagesEmitterName } from "../plugins"
 export default (() => {
   const Head: QuartzComponent = ({
     cfg,
@@ -31,9 +31,10 @@ export default (() => {
     const socialUrl =
       fileData.slug === "404" ? url.toString() : joinSegments(url.toString(), fileData.slug!)
 
-    const usesCustomOgImage = ctx.cfg.plugins.emitters.some(
-      (e) => e.name === CustomOgImagesEmitterName,
-    )
+//    const usesCustomOgImage = ctx.cfg.plugins.emitters.some(
+//      (e) => e.name === CustomOgImagesEmitterName,
+//    )
+    const ogImagePath = `https://${cfg.baseUrl}/static/og-image.png`
     const ogImageDefaultPath = `https://${cfg.baseUrl}/static/og-image.png`
 
     const coreStylesheet = css[0]?.content
